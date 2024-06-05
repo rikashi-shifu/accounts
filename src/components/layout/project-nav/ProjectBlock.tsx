08481c3d246a019d0f3bd149699925ec2e7e9085
rikashi-shifu <harryliow229@gmail.com>
2024-06-05 14:16:28 +0800

    Feat: Added tooltip for project block

--- src/components/layout/project-nav/ProjectBlock.tsx
+++ src/components/layout/project-nav/ProjectBlock.tsx
@@ -1,6 +1,7 @@
 import { useParams, useRouter } from "next/navigation";
 import React from "react";
 import { useNavContext } from "../Layout";
+import { Tooltip } from "@mui/material";
 
 interface Account {
   name: string;
@@ -26,21 +27,23 @@ const ProjectBlock: React.FC<ProjectBlockProps> = ({ project }) => {
 
   const formattedProjectName = project.name.toLowerCase().replace(/ /g, "-");
   return (
-    <button
-      onClick={() => {
-        setShowNav(true);
-        if (param.project !== formattedProjectName) {
-          router.push(`/${formattedProjectName}`);
-        }
-      }}
-      className={`${
-        param.project === formattedProjectName
-          ? "rounded-xl border-neutral-500"
-          : "rounded-full border-transparent"
-      } bg-[#2c2c2c] text-white hover:border-neutral-500 duration-150 border flex justify-center items-center w-14 h-14`}
-    >
-      {project.name.slice(0, 1)}
-    </button>
+    <Tooltip title={project.name} placement="right" arrow>
+      <button
+        onClick={() => {
+          setShowNav(true);
+          if (param.project !== formattedProjectName) {
+            router.push(`/${formattedProjectName}`);
+          }
+        }}
+        className={`${
+          param.project === formattedProjectName
+            ? "rounded-xl border-neutral-500"
+            : "rounded-full border-transparent"
+        } bg-[#2c2c2c] text-white hover:border-neutral-500 duration-150 border flex justify-center items-center w-14 h-14`}
+      >
+        {project.name.slice(0, 1)}
+      </button>
+    </Tooltip>
   );
 };
 
