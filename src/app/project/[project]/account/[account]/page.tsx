8dc2315e4600357587e61c52248489d615dbdd20
rikashi-shifu <harryliow229@gmail.com>
2024-06-10 15:16:21 +0800

    To: Added account's table

--- src/app/project/[project]/account/[account]/page.tsx
+++ src/app/project/[project]/account/[account]/page.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { DataTable } from "./data";
+import { Payment, columns } from "./columns";
 
-const Account = () => {
-  return <div>Account</div>;
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+const Account = async () => {
+  const data = await getData();
+
+  return (
+    <div className="p-10">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 };
 
 export default Account;
