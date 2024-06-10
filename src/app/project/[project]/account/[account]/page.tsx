d2f20d15eb4c1b07695fc72f4ef6a6a684da0def
rikashi-shifu <harryliow229@gmail.com>
2024-06-10 16:01:08 +0800

    To: Added account's table

--- src/app/project/[project]/account/[account]/page.tsx
+++ src/app/project/[project]/account/[account]/page.tsx
@@ -21,6 +21,27 @@ const Account = async () => {
   return (
     <div className="p-10">
       <DataTable columns={columns} data={data} />
+      <div className="border p-5 rounded-md flex">
+        {/* left */}
+        <div className="w-1/2 border">
+          {/* header */}
+          <div className="w-full flex">
+            <div className="w-[10%] border">
+              <input type="checkbox" />
+            </div>
+            <div className="w-[10%] border">Date</div>
+            <div className="w-[10%] border">Details</div>
+            <div className="w-[10%] border">Folio</div>
+            <div className="w-[10%] border">Amount</div>
+            <div className="w-[10%] border"></div>
+          </div>
+        </div>
+        {/* right */}
+        <div className="w-1/2 p-5 border">
+          {/* header */}
+          <div></div>
+        </div>
+      </div>
     </div>
   );
 };
